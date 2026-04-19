801c9fd37a1a785c524cef0f40e58cd4dfe26b6b
amar <amarnadh.t@hotmail.com>
2026-04-19 14:02:26 +0530

    trying to build VPC module - igw and rt added

--- vpc/main.tf
+++ vpc/main.tf
@@ -32,3 +32,49 @@ resource "aws_subnet" "private" {
     Name = "${var.env}-${each.key}"
   }
 }
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.env}-igw"
+  }
+}
+
+# 1. Create the Public Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "${var.env}-public-rt"
+  }
+}
+
+# 2. Associate ALL public subnets to this table
+resource "aws_route_table_association" "public" {
+  for_each       = aws_subnet.public
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.public.id
+}
+
+# 1. Create the Private Route Table
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  # Note: No 0.0.0.0/0 route yet unless you add a NAT Gateway
+  tags = {
+    Name = "${var.env}-private-rt"
+  }
+}
+
+# 2. Associate ALL private subnets to this table
+resource "aws_route_table_association" "private" {
+  for_each       = aws_subnet.private
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.private.id
+}
